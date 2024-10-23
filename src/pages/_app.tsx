2c87a2d135b5901e1426b97778cd5fcde12762bd
ShikshaFinder <janiharsh794@gmail.com>
2024-10-24 00:41:15 +0530

    index updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />;
       <Footer />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import Image from "next/image";
+import Carousel from "@/components/carousel";
+import Announcement from "@/components/Announcement";
+import AboutLdce from "@/components/AboutLdce";
+import Events from "@/components/Events";
+import LDCENumbers from "@/components/LDCENumbers";
+import AcadmicUnit from "@/components/AcadmicUnit";
+import Gallary from "@/components/Gallary";
+import Achivements from "@/components/Achivements";
+import News from "@/components/News";
+import Placements from "@/components/placements";
 
 export default function Home() {
   return (
    <>
-   Index</>
+   <Carousel />
+   <Announcement />
+    <AboutLdce />
+    <Events />
+    <LDCENumbers />
+    <AcadmicUnit />
+    <Gallary />
+    <Achivements />
+    <News />
+    <Placements />
+   </>
   );
 }
