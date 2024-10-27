1c5e6b9fa49eaa3443fdbfb99cd8e4159f26501d
Vishvam Moliya <vishvammoliya@gmail.com>
2024-10-28 02:14:42 +0530

    feat-UI: added CAROUSEL component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Carousel from "@/components/carousel";
 import Announcement from "@/components/Announcement";
 import AboutLdce from "@/components/AboutLdce";
 import Events from "@/components/Events";
@@ -9,21 +7,22 @@ import Gallary from "@/components/Gallary";
 import Achivements from "@/components/Achivements";
 import News from "@/components/News";
 import Placements from "@/components/placements";
+import Carousel from "@/components/carousel";
 
 export default function Home() {
   return (
-   <>
-   <Carousel />
-   <Announcement />
-    <AboutLdce />
-    <Events />
-    <LDCENumbers />
-    <AcadmicUnit />
-    <Gallary />
-    <Achivements />
-    <News />
-    <Placements />
-  Rudra panchal : Footer & Department page layout
-   </>
+    <>
+      <Carousel />
+      <Announcement />
+      <AboutLdce />
+      <Events />
+      <LDCENumbers />
+      <AcadmicUnit />
+      <Gallary />
+      <Achivements />
+      <News />
+      <Placements />
+      Rudra panchal : Footer & Department page layout
+    </>
   );
 }
