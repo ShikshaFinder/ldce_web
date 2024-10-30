e871fea6a219fa7929b510d9467eab92b982ed72
ShikshaFinder <janiharsh794@gmail.com>
2024-10-30 13:36:21 +0530

    animation effects corrected in Event Card

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -14,24 +14,26 @@ function EventCard({
   return (
     <div className="relative group" style={{ height: '500px' }}>
       <img
-        src={img}
-        className="w-full object-cover h-full"
-        alt="image related with events"
+      src={img}
+      className="w-full object-cover h-full"
+      alt="image related with events"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-4">
+      <div className="mb-4">
         <a
-          href={link}
-          className="text-xl font-bold text-white mb-2 hover:text-primary"
+        href={link}
+        className="text-3xl text-left font-bold text-white mb-2 block"
         >
-          {name}
+        {name}
         </a>
-        <p className="text-white text-center mb-4">{discription}</p>
-        <a
-          className="btn-hover-bg bg-primary text-white py-2 px-4 rounded hover:bg-opacity-80"
-          href={link}
-        >
-          Read More
-        </a>
+        <p className="text-white text-left">{discription}</p>
+      </div>
+      <a
+        className="btn-hover-bg bg-primary text-white py-2 px-4 rounded transform translate-y-4 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300"
+        href={link}
+      >
+        Read More
+      </a>
       </div>
     </div>
   );
